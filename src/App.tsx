d55cc1d7e3e47a64eb99170084ebc8834e78910b
Tanjila Akhtar <37596431+tanjila-tisha@users.noreply.github.com>
2023-01-15 16:45:55 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import DeliveryOptions from './components/DeliveryOptions';
-
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DeliveryOptions from "./components/DeliveryOptions";
+import Confirmation from "./components/Confirmation";
+import Box from "@mui/material/Box";
 
 function App() {
   return (
-    <div className="App">
-      <DeliveryOptions />
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Box>
+        <Router>
+          <Routes>
+            <Route path="/confirmation" element={<Confirmation />}></Route>
+            <Route path="/" element={<DeliveryOptions />}></Route>
+          </Routes>
+        </Router>
+      </Box>
+    </Box>
   );
 }
 
