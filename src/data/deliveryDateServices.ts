5e97a22d91131b92a9d71a7588510238b5d8a11f
Tanjila Akhtar <37596431+tanjila-tisha@users.noreply.github.com>
2023-01-15 16:44:34 +0100

    Update deliveryDateServices.ts

--- src/data/deliveryDateServices.ts
+++ src/data/deliveryDateServices.ts
@@ -1,6 +1,16 @@
+// Fetch dates
 export const getDeliveryDates = async () => {
   const response = await fetch(
     "https://api.mathem.io/mh-test-assignment/delivery/dates"
+  );
+  const data = await response.json();
+  return data;
+};
+
+// Fetch time slots for given dates
+export const getAvailableTimeSlots = async (selectedDate: string) => {
+  const response = await fetch(
+    `https://api.mathem.io/mh-test-assignment/delivery/times/${selectedDate}`
   );
   const data = await response.json();
   return data;
